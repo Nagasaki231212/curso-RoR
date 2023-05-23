a5bd06163f1b71149069dff4073b0b935eedf402
Oscar Gil <oscargil19971212@gmail.com>
2023-05-23 14:33:40 -0400

    estancado en crear tarea

---
+++ app/models/task.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  due_date    :date
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_tasks_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
+class Task < ApplicationRecord
+  belongs_to :category
+  belongs_to :owner, class_name: 'User'
+
+  validates :name, :description, presence: true  
+  validates :name, uniqueness: { case_insensitive: false }
+  validate  :due_date_validity
+
+  def due_date_validity
+    return if due_date.blank?
+    return if due_date > Date.today
+    errors.add :due_date, I18n.t('task.errors.invalid_due_date') 
+  end 
+end
